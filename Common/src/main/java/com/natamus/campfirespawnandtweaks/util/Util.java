ac7896fef3001686107afad6cfd591fb8f6ceaa7
Rick South <38502394+ricksouth@users.noreply.github.com>
2026-06-07 19:37:54 +0200

    (v4.3) Updated the mod's text to use Collective's new translation system, which adds many languages alongside English! [+1]

--- Common/src/main/java/com/natamus/campfirespawnandtweaks/util/Util.java
+++ Common/src/main/java/com/natamus/campfirespawnandtweaks/util/Util.java
@@ -122,7 +122,7 @@ public class Util {
 						continue;
 					}
 					
-					MessageFunctions.sendMessage(target, "Campfire spawn point removed.", ChatFormatting.DARK_GRAY);
+					MessageFunctions.sendTranslatableMessage(target, "collective.campfirespawnandtweaks.message.campfirespawnpointremoved", ChatFormatting.DARK_GRAY);
 				}
 			}
 		}
